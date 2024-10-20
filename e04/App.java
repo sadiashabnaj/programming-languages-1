b3277547b6316feca9f4d719ffb43a8cd14f674a
sadiashabnaj <sadiashabnj2710@gmail.com>
2024-10-20 23:09:48 +0300

    eo4 complete

---
+++ e04/App.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter a name: ");
+        String name = scanner.nextLine();
+
+        System.out.println("You entered: " + name);
+
+        for (int i = name.length() - 1; i >= 0; i -= 2) {
+            System.out.println(name.charAt(i));
+        }
+
+        scanner.close();
+    }
+}
